67e48069552eb50d4bfa8224100dd11ef7f57325
8eatles <8eatles@naver.com>
2020-03-22 21:37:51 +0900

    DB Config Error fix

--- src/server/db/models/index.js
+++ src/server/db/models/index.js
@@ -4,19 +4,7 @@ const Sequelize = require('sequelize');
 const config = require('../../config/db');
 
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  );
-}
-
+const sequelize = new Sequelize(config[process.env.NODE_ENV]);
 const basename = path.basename(__filename);
 
 fs.readdirSync(__dirname)
