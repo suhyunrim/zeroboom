6fd0b0db3169437b003b8c865775b8080d0d3bd6
RimSuHyun <rsh9210@bluehole.net>
2020-04-15 20:41:07 +0900

    bodyParser 추가

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const loader = require('./loaders');
 const { port } = require('./config');
 const { logger } = require('./loaders/logger');
 
 const startServer = async () => {
   const app = express();
+  app.use(bodyParser.urlencoded({extended: false}));
+
   const server = await loader(app);
 
   server.listen(port, (err) => {
