455277d7921db602334bdd039bf273068cc569f5
RimSuHyun <rsh9210@bluehole.net>
2020-04-19 01:40:27 +0900

    그룹 랭킹 가져오는 기능 추가

--- src/api/routes/group.js
+++ src/api/routes/group.js
@@ -16,4 +16,42 @@ module.exports = (app) => {
       res.json({ result: rows[1] ? 'succeed' : 'already exist' });
     });
   });
+
+  route.get('/ranking', async (req, res) => {
+    const { groupName } = req.body;
+
+    if (!groupName)
+      return res.json({ result: 'invalid group name' });
+
+    const group = await models.group.findOne({ where: { groupName } });
+    if (!group)
+      return res.json({ result: 'group is not exist'} );
+
+    let users = await models.user.findAll({ where: { groupId: group.id } });
+    users = users.filter(elem => elem.win + elem.lose >= 4);
+    users.sort((a, b) => (b.defaultRating + b.additionalRating) - (a.defaultRating + a.additionalRating));
+
+    const userIds = users.map((elem) => elem.riotId);
+    const summoners = await models.summoner.findAll({ where: { riotId: userIds } });
+    const summonerObj = summoners.reduce((obj, v) => {
+      obj[v.riotId] = v;
+      return obj;
+    }, {});
+
+    let result = users.map((elem) => {
+      return {
+        riotId: elem.riotId,
+        rating: elem.defaultRating + elem.additionalRating,
+        win: elem.win,
+        lose: elem.lose,
+        winRate: Math.ceil((elem.win / (elem.win + elem.lose)) * 100),
+      }
+    });
+
+    result.forEach((user) => {
+      user.name = summonerObj[user.riotId].name;
+    });
+
+    return res.json( { result: result });
+  })
 };
