3650be2b5b538f69a551669624b49d2817701701
enterkeywasremoved <enterkeywasremoved@gmail.com>
2020-05-20 19:55:38 +0900

    add : match-make command

---
+++ src/commands/match-make.js
@@ -0,0 +1,18 @@
+const matchController = require('../../controller/match');
+
+exports.run = (message, args) => {
+	const result = await matchController.generateMatch('휘핑크림', [], [], args);
+
+	return JSON.stringify(result);
+}
+
+exports.conf = {
+	enabled: true,
+	aliases: ['매칭생성', '자동매칭', 'mm'],
+};
+
+exports.help = {
+	name: 'match-make',
+	description: 'rating-based auto matching.',
+	usage: '/match-make [command]'
+};
