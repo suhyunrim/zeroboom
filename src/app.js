035276d8eafca4f3452d8e0510f138f9ea4c3d7c
ZeroBoom <rsh921015@naver.com>
2020-07-14 17:46:53 +0900

    Revert "project reset"
    This reverts commit d76df36cd13f499bc8b91138a2d2fb20a132d505.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const loader = require('./loaders');
+const { port } = require('./config');
 const { logger } = require('./loaders/logger');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/token');
+const friendlyGameRouter  = require('./routes/friendly-game');
 
 const startServer = async () => {
   const app = express();
+  app.use(bodyParser.urlencoded({extended: false}));
 
-  await loader(app);
+  app.use(express.static(path.join(__dirname, '/../public')));
+  app.set('views', path.join(__dirname, '/../views'));
+  app.set('view engine', 'ejs');
+  app.engine('html', require('ejs').renderFile)
+
+  app.use('/', indexRouter);
+  app.use('/token', loginRouter)
+  app.use('/friendly-game', friendlyGameRouter);
+
+  const server = await loader(app);
+
+  server.listen(port, (err) => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+    }
+    logger.info(`
+      ################################################
+      Hello, Camille! Server listening on port: ${port}
+      ################################################
+    `);
+  });
 };
 
 startServer();
--- src_legacy/config/db.js
+++
@@ -1,7 +0,0 @@
-const { database } = require('./index');
-
-module.exports = {
-  development: database,
-  test: database,
-  production: database,
-};
