838b9c31eeda0e5791bcdffb8b8e023cc9dda6e5
ZeroBoom <rsh921015@naver.com>
2023-02-21 00:37:58 +0900

    보이스 채널에서 인원 뽑기 할 때 한번 짤린 사람은 다음 뽑기 때 확정으로 뽑히게 처리

--- src/commands/pick-users.js
+++ src/commands/pick-users.js
@@ -1,13 +1,44 @@
-const { registerUser } = require('../services/user');
+const moment = require('moment');
+
+const pickCount = 10;
+const fixedMember = {};
 
 exports.run = async (groupName, interaction) => {
   if (!interaction.member.voice.channelId) {
     return '입장해있는 음성채널이 없습니다.';
   }
 
+  if (!fixedMember[groupName]) {
+    fixedMember[groupName] = {};
+  }
+
+  let pickedUsers = [];
   const members = interaction.member.voice.channel.members;
-  const pickedUsers = members.random(10);
-  const unpickedUsers = members.filter((member) => !pickedUsers.includes(member));
+  // 12시간 안에 한번 짤린 유저가 있으면 무조건 포함시킴 (by zeroboom)
+  for (let pair of members) {
+    const member = pair[1];
+    const tagetUserMoment = fixedMember[groupName][member.id];
+    if (tagetUserMoment) {
+      const diff = moment().utc().diff(tagetUserMoment, 'hours');
+      if (diff < 12) {
+        pickedUsers.push(member);
+        delete fixedMember[groupName][member.id];
+      }
+    }
+  }
+
+  const fixedNicknames = pickedUsers.map((member) => {
+    const nickname = member.nickname != null ? member.nickname : member.user.username;
+    const startIndex = nickname.indexOf('(');
+    return nickname.substring(startIndex + 1, nickname.length - 1);
+  });
+
+  pickedUsers = pickedUsers.concat(members.filter(member => !pickedUsers.includes(member)).random(pickCount - pickedUsers.length));
+
+  const unpickedUsers = members.filter((member) => !pickedUsers.includes(member)).map((member) => member);
+  for (let unpickedUser of unpickedUsers) {
+    fixedMember[groupName][unpickedUser.id] = moment().utc();
+  }
 
   const pickedNicknames = pickedUsers.map((member, index) => {
     const nickname = member.nickname != null ? member.nickname : member.user.username;
@@ -27,16 +58,16 @@ exports.run = async (groupName, interaction) => {
     return nickname.substring(startIndex + 1, nickname.length - 1);
   });
 
-  let message = `**${interaction.member.voice.channel.name}** 채널에서 **${members.size}명** 중 **${10}명**을 뽑습니다!
+  let message = `**${interaction.member.voice.channel.name}** 채널에서 **${members.size}명** 중 **${pickCount}명**을 뽑습니다!
 
    \`🎉 축하합니다! 🎉\`
-   :white_check_mark:: ${pickedNicknames.join(', ')}
+   :white_check_mark:: ${pickedNicknames.map(name => fixedNicknames.includes(name) ? `${name}(확정)` : name).join(', ')}
    :robot:: /매칭생성 ${commandStr.join(' ')}`;
 
   if (unpickedNicknames.length > 0) {
     message += `
     ---------------------------------------
-    ❌: ${unpickedNicknames.join(',')}`;
+    ❌: ${unpickedNicknames.join(',')} (다음 뽑기 때 확정으로 뽑히게 됩니다)`;
   }
 
   return message;
